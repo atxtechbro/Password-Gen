496a3617b5978756e8e697e26206cf800f1310e1
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2021-08-24 10:20:15 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='password-gen',
+    version='1.0.0',
+    author='Morgan Joyce',
+    author_email='morganj2k@gmail.com',
+    packages=find_packages()
+)
